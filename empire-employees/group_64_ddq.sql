f49ebc919f176529b35d7af7e43a331e090ca71a
Jeremy Tsang <jeremy@jeremytsang.com>
2020-07-24 18:19:16 -0700

    Add CREATE TABLE for garrisons and loadouts

--- empire-employees/group_64_ddq.sql
+++ empire-employees/group_64_ddq.sql
@@ -1,13 +1,31 @@
 CREATE TABLE troopers (
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+DROP TABLE IF EXISTS `garrisons`;
 CREATE TABLE garrisons (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  capacity INT(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `garrisons` (`name`, `capacity`) VALUES
+("Tatooine", 10000),
+("Coruscant", 100000),
+("Kashyyk", 5000);
 
+
+DROP TABLE IF EXISTS `loadouts`;
 CREATE TABLE loadouts (
+  id INT AUTO_INCREMENT,
+  blaster VARCHAR(255) NOT NULL,
+  detonator VARCHAR(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `loadouts` (`blaster`, `detonator`) VALUES
+("EL-16", "Thermal"),
+("E-11", "Fragmentation"),;
 
 
 CREATE TABLE ships (
