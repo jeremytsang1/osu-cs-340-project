796a6c06120cc316b37e40d687d9919691cb325a
Jeremy Tsang <jeremy@jeremytsang.com>
2020-08-02 21:31:19 -0700

    Implement backend for droid INSERT query

--- empire-employees/droids.js
+++ empire-employees/droids.js
@@ -1,6 +1,28 @@
 module.exports = function() {
   let express = require('express');
   let router = express.Router();
+  const DROID_TYPES = [
+    "Assassin",
+    "Astromech",
+    "Battel",
+    "Biological Science",
+    "Child Care",
+    "Engineering",
+    "Environmental",
+    "Exploration",
+    "General Labor",
+    "Gladiator",
+    "Hazardous Service",
+    "Labor Specialist",
+    "Maintenance",
+    "Mathematics",
+    "Medical",
+    "Physical Science",
+    "Protocol",
+    "Security",
+    "Servant",
+    "Tutor",
+  ];
 
   function getDroids(res, mysql, context, complete) {
     mysql.pool.query("SELECT id, type from droids;", function(error, results, fields) {
@@ -32,6 +54,39 @@ module.exports = function() {
 	res.render('droids', context);
       }
     }
+  });
+
+  // add a new droid to the table
+  router.post('/', function(req, res) {
+    let mysql = req.app.get('mysql');
+    let sql = "INSERT INTO `droids` (id, `type`) VALUE (?, ?);";
+    let inserts = [req.body.id, req.body.type];
+
+    // validate droid type
+    if (!DROID_TYPES.includes(req.body.type)) {
+      // do not bother saving to database if droid type has been tampered with
+      res.redirect('/droids?valid_type=false')
+      res.end();
+    }
+
+    // attempt the INSERT query
+    sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+      if (error) { // failed INSERT query
+	if (error.code === "ER_DUP_ENTRY") { // Duplicate ID
+	  // redirect if ID was found to be non-unique
+	  res.redirect('/droids?valid_id=false');
+	  res.end();
+	}
+
+	// failed for reason other than duplicate ID
+	console.log(JSON.stringify(error));
+	res.write(JSON.stringify(error));
+	res.end();
+
+      } else {  // successful INSERT query
+	res.redirect('/droids');
+      }
+    });
   });
 
   return router;
