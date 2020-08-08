89a5366e9cf541062504d24fe4545854be9f14bf
Jeremy Tsang <jeremy@jeremytsang.com>
2020-08-08 06:29:22 -0700

    Add extraparams to query string in helper function

--- empire-employees/queryHelpers.js
+++ empire-employees/queryHelpers.js
@@ -1,23 +1,24 @@
 const Validator = require('./validator.js');
 
-function attemptQuery(req, res, mysql, sql, inserts, expectedErrorHandlers, baseRoute) {
+function attemptQuery(req, res, mysql, sql, inserts,
+  expectedErrorHandlers, baseRoute, extraQueryParams = "") {
   mysql.pool.query(sql, inserts.map(elt => elt.value), (error, results, fields) => {
     if (error) { // query failure
-      handleFailedQuery(res, error, expectedErrorHandlers, baseRoute);
+      handleFailedQuery(res, error, expectedErrorHandlers, baseRoute, extraQueryParams);
     } else { // query success
       // console.log(results);
       let queryString = Validator.handleZeroAffectedRows(results);
       queryString = (queryString === "") ? queryString : `?${queryString}`
-      res.redirect(`${baseRoute}${queryString}`); // NOTE: '?' not harded coded
+      res.redirect(`${baseRoute}${queryString}${extraQueryParams}`); // NOTE: '?' not harded coded
     }
   });
 }
 
-function handleFailedQuery(res, error, expectedErrorHandlers, baseRoute) {
+function handleFailedQuery(res, error, expectedErrorHandlers, baseRoute, extraQueryParams = "") {
   let code = error.code;
 
   if (expectedErrorHandlers.hasOwnProperty(code)) {  // error was expected
-    res.redirect(`${baseRoute}?${expectedErrorHandlers[code](res, error)}`);
+    res.redirect(`${baseRoute}?${expectedErrorHandlers[code](res, error)}${extraQueryParams}`);
   } else {                                           // error was unepected
     let stringifiedError = JSON.stringify(error);
     console.log(stringifiedError);
